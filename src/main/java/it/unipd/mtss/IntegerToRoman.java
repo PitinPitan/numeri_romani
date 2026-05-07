325983c38b86470a95dc10673b38846579619f77
PitinPitan <131447362+PitinPitan@users.noreply.github.com>
2026-05-07 10:56:06 +0200

    Modifica file .java
    Modifica corpo file

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,10 @@
+package it.unipd.mtss;
+
+
+public class RomanInteger
+{
+    public static String convert(int number) {
+        // TODO: Implementare la logica per i primi 3 numeri
+        return "";
+    }
+}
