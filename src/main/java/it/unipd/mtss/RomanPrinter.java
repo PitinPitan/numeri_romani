77f834c1c50cde3d96c2c5fc31c6e5e55011b731
Milan Mattia <vs.spiderscip@gmail.com>
2026-05-08 13:53:35 +0200

    Correzzione del codice per il corretto raggiungimento di almeno 85%

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,16 @@
+/////////////////////////////////
+// MATTIA MILAN 2137968
+// NICOLA LUGATO 2145605
+/////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+  public static String print(int num){
+    return printAsciiArt(IntegerToRoman.convert(num));
+  }
+
+  private static String printAsciiArt(String romanNumber){
+    // TODO
+    return "";
+  }
+}
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,11 +1,19 @@
 /////////////////////////////////
-// // MATTIA MILAN 2137968
-// // NICOLA LUGATO 2145605
-// /////////////////////////////////
+// MATTIA MILAN 2137968
+// NICOLA LUGATO 2145605
+/////////////////////////////////
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 
 public class IntegerToRomanTest {
 
-   
+    @Test
+    public void testConstructor() {
+        // Questo serve solo per coprire il costruttore implicito della classe
+        IntegerToRoman test = new IntegerToRoman();
+        assertNotNull(test);
+    }
 }
